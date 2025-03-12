29fc51776bdb3524ae536452241c5db46a6435f3
P4r1nc3 <konrad.tupta@gmail.com>
2025-03-12 15:11:59 +0100

    Created EmailService

---
+++ src/main/java/com/flashdash/notification/service/EmailService.java
@@ -0,0 +1,30 @@
+package com.flashdash.notification.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Async
+    public void sendEmail(String to, String subject, String content) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(content);
+            message.setFrom("no-reply@flashdash.com");
+            mailSender.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
