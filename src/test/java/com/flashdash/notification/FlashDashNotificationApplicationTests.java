8fb5c17b810a9155582859c9533cc9de3749f507
P4r1nc3 <konrad.tupta@gmail.com>
2025-03-12 08:42:39 +0100

    Initial commit

---
+++ src/test/java/com/flashdash/notification/FlashDashNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashdash.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashDashNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
