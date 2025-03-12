f28baafa25222b774a653e8a668b5297ec1e185e
P4r1nc3 <konrad.tupta@gmail.com>
2025-03-12 08:59:02 +0100

    Created JwtManager

---
+++ src/main/java/com/flashdash/notification/config/JwtManager.java
@@ -0,0 +1,38 @@
+package com.flashdash.notification.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Component
+public class JwtManager {
+
+    private final Key secretKey;
+
+    public JwtManager(@Value("${jwt.secret}") String secret) {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
+
+    public String extractUserFrn(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
