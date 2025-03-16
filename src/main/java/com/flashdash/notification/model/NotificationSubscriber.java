57b6fce405ff5ffe6dc68ab062a4d499756f55e9
P4r1nc3 <konrad.tupta@gmail.com>
2025-03-16 12:21:38 +0100

    Created NotificationSubscriber model

---
+++ src/main/java/com/flashdash/notification/model/NotificationSubscriber.java
@@ -0,0 +1,38 @@
+package com.flashdash.notification.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "notification_subscribers")
+public class NotificationSubscriber {
+
+    @Id
+    @Column(name = "user_frn", nullable = false, length = 256)
+    private String userFrn;
+
+    @Column(name = "email", nullable = false, length = 256)
+    private String email;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    @Column(name = "notification_time", nullable = false)
+    private LocalDateTime notificationTime;
+
+    @Column(name = "daily_notifications", nullable = false)
+    private boolean dailyNotifications;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "notification_channel", nullable = false)
+    private NotificationChannel notificationChannel;
+
+    public NotificationSubscriber() {}
+}
